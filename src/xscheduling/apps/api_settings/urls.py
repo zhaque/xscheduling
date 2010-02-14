c509bd648d6e962c5f53ae707ece680c9d29934d
Stepan Rakhimov <strakh@macbook2.local>
2010-02-14 03:01:27 +0300

    ajax-based enable/disable links

--- src/xscheduling/apps/api_settings/urls.py
+++ src/xscheduling/apps/api_settings/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^$', 'api_settings.views.list_api', name='api_settings-list'),
+    url(r'^(?P<object_id>\d+)/enable/$', 'api_settings.views.enable_api', name='api_settings-enable'),
+    url(r'^(?P<object_id>\d+)/disable/$', 'api_settings.views.disable_api', name='api_settings-disable'),
 )
--- src/xscheduling/apps/api_settings/views.py
+++ src/xscheduling/apps/api_settings/views.py
@@ -1,5 +1,7 @@
 from django.views.generic.simple import direct_to_template
 from django.contrib.auth.decorators import login_required
+from django.core import serializers
+from django.http import HttpResponse
 from django.utils.text import capfirst
 from django.utils.translation import ugettext as _
 from api_settings.models import Api
@@ -10,3 +12,26 @@ def list_api(request):
   context_vars['header'] = capfirst(_('user API list'))
   context_vars['apis'] = Api.objects.filter(user=request.user)
   return direct_to_template(request, template='api_settings/list.html', extra_context=context_vars)
+
+@login_required
+def enable_api(request, object_id):
+  if request.method == "POST":
+    api = Api.objects.get(id=object_id)
+    if not api.enabled:
+      api.enabled = True
+      api.save()
+    api = Api.objects.filter(id=object_id)
+    json = serializers.serialize('json', api)
+    return HttpResponse(json, mimetype="application/json")
+
+@login_required
+def disable_api(request, object_id):
+  if request.method == "POST":
+    api = Api.objects.get(id=object_id)
+    if api.enabled:
+      api.enabled = False
+      api.save()
+    api = Api.objects.filter(id=object_id)
+    json = serializers.serialize('json', api)
+    return HttpResponse(json, mimetype="application/json")
+  
