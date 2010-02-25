d507ba75bcb01af13d056f580a5351e535eedd49
Stepan Rakhimov <strakh@macbook2.local>
2010-02-25 14:20:45 +0300

    added staff CRU

--- src/xscheduling/apps/schedule/urls.py
+++ src/xscheduling/apps/schedule/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url(r'^client/$', 'schedule.views.list_clients', name='schedule-list'),
+    url(r'^$', 'schedule.views.root', name='schedule-root'),
+    url(r'^client/$', 'schedule.views.list_clients', name='schedule-client-list'),
     url(r'^client/add/$', 'schedule.views.add_client', name='schedule-client-add'),
     url(r'^client/(?P<object_id>\d+)/$', 'schedule.views.get_client', name='schedule-client'),
     url(r'^client/(?P<object_id>\d+)/edit/$', 'schedule.views.edit_client', name='schedule-client-edit'),
     url(r'^client/(?P<object_id>\d+)/contact/add/$', 'schedule.views.add_contact', name='schedule-contact-add'),
     url(r'^contact/(?P<object_id>\d+)/$', 'schedule.views.get_contact', name='schedule-contact'),
     url(r'^contact/(?P<object_id>\d+)/edit/$', 'schedule.views.edit_contact', name='schedule-contact-edit'),
+    url(r'^staff/$', 'schedule.views.list_staff', name='schedule-staff-list'),
+    url(r'^staff/add/$', 'schedule.views.add_staff', name='schedule-staff-add'),
+    url(r'^staff/(?P<object_id>\d+)/$', 'schedule.views.get_staff', name='schedule-staff'),
+    url(r'^staff/(?P<object_id>\d+)/edit/$', 'schedule.views.edit_staff', name='schedule-staff-edit'),
 )
--- src/xscheduling/apps/schedule/views.py
+++ src/xscheduling/apps/schedule/views.py
@@ -4,9 +4,15 @@ from django.http import HttpResponseRedirect
 from django.utils.text import capfirst
 from django.utils.translation import ugettext as _
 from django.views.generic.simple import direct_to_template
+from uni_form.helpers import FormHelper, Submit, Reset
 from workflowmax.client.models import Client, Contact
 from workflowmax.client.forms import ClientForm, ContactForm
-from uni_form.helpers import FormHelper, Submit, Reset
+from workflowmax.staff.models import Staff
+from workflowmax.staff.forms import StaffForm
+
+def root(request):
+  return direct_to_template(request, template='schedule/root.html')
+# Client views
 
 def list_clients(request):
   context_vars = dict()
@@ -18,7 +24,7 @@ def get_client(request, object_id):
   try:
     object_id = int(object_id)
   except ValueError:
-    return HttpResponseRedirect(reverse('schedule-list'))
+    return HttpResponseRedirect(reverse('schedule-client-list'))
   context_vars = dict()
   context_vars['header'] = '%s %d' % (capfirst(_('client')), object_id)
   context_vars['client'] = Client.objects.get(id=object_id)
@@ -28,7 +34,7 @@ def get_contact(request, object_id):
   try:
     object_id = int(object_id)
   except ValueError:
-    return HttpResponseRedirect(reverse('schedule-list'))
+    return HttpResponseRedirect(reverse('schedule-client-list'))
   context_vars = dict()
   context_vars['header'] = '%s %d' % (capfirst(_('contact')), object_id)
   context_vars['contact'] = Contact.objects.get(id=object_id)
@@ -38,7 +44,7 @@ def edit_client(request, object_id):
   try:
     object_id = int(object_id)
   except ValueError:
-    return HttpResponseRedirect(reverse('schedule-list'))
+    return HttpResponseRedirect(reverse('schedule-client-list'))
   context_vars = dict()
   context_vars['header'] = '%s %d' % (capfirst(_('client')), object_id)
   client = Client.objects.get(id=object_id)
@@ -94,7 +100,7 @@ def add_contact(request, object_id):
   try:
     object_id = int(object_id)
   except ValueError:
-    return HttpResponseRedirect(reverse('schedule-list'))
+    return HttpResponseRedirect(reverse('schedule-client-list'))
   context_vars = dict()
   context_vars['header'] = capfirst(_('add new contact for client %d') % object_id)
   form = ContactForm()
@@ -123,7 +129,7 @@ def edit_contact(request, object_id):
   try:
     object_id = int(object_id)
   except ValueError:
-    return HttpResponseRedirect(reverse('schedule-list'))
+    return HttpResponseRedirect(reverse('schedule-client-list'))
   context_vars = dict()
   context_vars['header'] = capfirst(_('edit contact %d') % object_id)
   contact = Contact.objects.get(id=object_id)
@@ -147,5 +153,74 @@ def edit_contact(request, object_id):
   
   return direct_to_template(request, template='schedule/form.html', extra_context=context_vars)
 
+# Staff views
 
+def list_staff(request):
+  context_vars = dict()
+  context_vars['header'] = capfirst(_('staff list'))
+  context_vars['staff_list'] = Staff.objects.all()
+  return direct_to_template(request, template='schedule/list.html', extra_context=context_vars)
 
+def get_staff(request, object_id):
+  try:
+    object_id = int(object_id)
+  except ValueError:
+    return HttpResponseRedirect(reverse('schedule-staff-list'))
+  context_vars = dict()
+  context_vars['header'] = '%s %d' % (capfirst(_('staff')), object_id)
+  context_vars['staff'] = Staff.objects.get(id=object_id)
+  return direct_to_template(request, template='schedule/view.html', extra_context=context_vars)
+
+def edit_staff(request, object_id):
+  try:
+    object_id = int(object_id)
+  except ValueError:
+    return HttpResponseRedirect(reverse('schedule-staff-list'))
+  context_vars = dict()
+  context_vars['header'] = capfirst(_('edit staff %d') % object_id)
+  staff = Staff.objects.get(id=object_id)
+  form = StaffForm(staff.to_dict())
+  helper = FormHelper()
+  helper.form_class = 'uniform'
+  submit = Submit('save',_('save'))
+  helper.add_input(submit)
+  context_vars['form'] = form
+  context_vars['helper'] = helper
+  if request.method == "POST":
+    form = StaffForm(request.POST, request.FILES)
+    if form.is_valid():
+      staff.name = form.cleaned_data['name']
+      staff.address = form.cleaned_data['address']
+      staff.phone = form.cleaned_data['phone']
+      staff.mobile = form.cleaned_data['mobile']
+      staff.email = form.cleaned_data['email']
+      staff.payrollcode = form.cleaned_data['payrollcode']
+      staff.save()
+      return HttpResponseRedirect(reverse('schedule-staff', args=[staff.id]))
+  
+  return direct_to_template(request, template='schedule/form.html', extra_context=context_vars)
+
+def add_staff(request):
+  context_vars = dict()
+  context_vars['header'] = capfirst(_('add new staff'))
+  form = StaffForm()
+  helper = FormHelper()
+  helper.form_class = 'uniform'
+  submit = Submit('save',_('save'))
+  helper.add_input(submit)
+  context_vars['form'] = form
+  context_vars['helper'] = helper
+  if request.method == "POST":
+    form = StaffForm(request.POST, request.FILES)
+    if form.is_valid():
+      staff = Staff()
+      staff.name = form.cleaned_data['name']
+      staff.address = form.cleaned_data['address']
+      staff.phone = form.cleaned_data['phone']
+      staff.mobile = form.cleaned_data['mobile']
+      staff.email = form.cleaned_data['email']
+      staff.payrollcode = form.cleaned_data['payrollcode']
+      staff = staff.save()
+      return HttpResponseRedirect(reverse('schedule-staff', args=[staff.id]))
+  
+  return direct_to_template(request, template='schedule/form.html', extra_context=context_vars)
