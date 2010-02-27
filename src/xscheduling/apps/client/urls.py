f00c3cea909329b99f5d60d8240841df7984a9fc
Stepan Rakhimov <strakh@macbook2.local>
2010-02-27 17:08:33 +0300

    client app urls

---
+++ src/xscheduling/apps/client/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    url(r'^$', 'client.views.list_clients', name='client-list'),
+    url(r'^add/$', 'client.views.add_client', name='client-add'),
+    url(r'^(?P<object_id>\d+)/$', 'client.views.get_client', name='client-view'),
+    url(r'^(?P<object_id>\d+)/edit/$', 'client.views.edit_client', name='client-edit'),
+    url(r'^(?P<object_id>\d+)/contact/add/$', 'client.views.add_contact', name='client-contact-add'),
+    url(r'^contact/(?P<object_id>\d+)/$', 'client.views.get_contact', name='client-contact-view'),
+    url(r'^contact/(?P<object_id>\d+)/edit/$', 'client.views.edit_contact', name='client-contact-edit'),
+    url(r'^(?P<object_id>\d+)/note/add/$', 'client.views.add_note', name='client-note-add'),
+    url(r'^note/(?P<object_id>\d+)/$', 'client.views.get_note', name='client-note-view'),
+    url(r'^note/(?P<object_id>\d+)/edit/$', 'client.views.edit_note', name='client-note-edit'),
+)
--- src/xscheduling/apps/client/views.py
+++ src/xscheduling/apps/client/views.py
@@ -1,1 +1,22 @@
-# Create your views here.
+from client.models import Client, Note, Contact, Address
+
+def list_clients(request):
+  pass
+def add_client(request):
+  pass
+def get_client(request. object_id):
+  pass
+def edit_client(request. object_id):
+  pass
+def add_contact(request. object_id):
+  pass
+def get_contact(request. object_id):
+  pass
+def edit_contact(request. object_id):
+  pass
+def add_note(request. object_id):
+  pass
+def get_note(request. object_id):
+  pass
+def edit_note(request. object_id):
+  pass
