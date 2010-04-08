1235aa0fd847ff3097e59d7de58ed2f0042cd495
Stepan Rakhimov <strakh@macbook2.local>
2010-04-09 00:12:56 +0400

    add job bugfix

--- src/xscheduling/apps/job/models.py
+++ src/xscheduling/apps/job/models.py
@@ -256,10 +256,9 @@ def check_staff(sender, instance, **kwargs):
 
 #post_save.connect(check_staff, sender=Job)
 
-from django.db.models.signals import pre_save
-
 def set_name(sender, instance, **kwargs):
   if not instance.name:
     instance.name = 'job_%06d' % instance.id
-
-pre_save.connect(set_name, sender=Job)
+    instance.save()
+  
+post_save.connect(set_name, sender=Job)
