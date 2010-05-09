f072068f60018ff3fdd864fe85f880f67281bf66
Stepan Rakhimov <strakh@macbook2.local>
2010-05-09 20:00:25 +0400

    date format change

--- src/xscheduling/settings.py
+++ src/xscheduling/settings.py
@@ -125,5 +125,5 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.messages.context_processors.messages",
 )
 
-DATE_INPUT_FORMATS = ('%m/%d/%Y',)
+DATE_INPUT_FORMATS = ('%d/%m/%Y',)
 TIME_INPUT_FORMATS = ('%H:%M',)
